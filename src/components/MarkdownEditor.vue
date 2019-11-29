d0b73aa3b710caa487980173957bf7361dfd300a
pengjq <cambridge_james@foxmail.com>
2019-11-29 14:32:04 +0800

    mavon-editor模块测试

---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="editor">
+    <mavon-editor style="height: 100%"></mavon-editor>
+  </div>
+</template>
+
+<script>
+  var mavonEditor = require('mavon-editor');
+  import 'mavon-editor/dist/css/index.css'
+
+  export default {
+    name: "MarkdownEditor",
+    components: {
+      'mavon-editor': mavonEditor.mavonEditor
+    }
+  }
+</script>
+
+<style scoped>
+  #editor {
+    margin: auto;
+    width: 80%;
+    height: 580px;
+  }
+</style>
---
+++ src/pages/hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <MarkdownEditor v-model="value"/>
+    </div>
+</template>
+
+<script>
+    import MarkdownEditor from "../components/MarkdownEditor";
+
+  export default {
+    name: "hello",
+    components: {MarkdownEditor},
+    created() {
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
